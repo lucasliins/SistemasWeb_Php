e8a2eb866d302fe37671ce3f574d1aeb7b3006ff
lucasliins <lucaslins_12@hotmail.com>
2023-04-22 17:29:22 -0300

    atualizacao de projeto

--- login.php
+++ login.php
@@ -12,14 +12,14 @@ try {
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   //echo "Connected successfully";
-  $stmt = $conn->prepare("SELECT FROM usuario WHERE lastname='Doe'");
+  $stmt = $conn->prepare("SELECT codigo FROM usuario WHERE email=:email AND senha=md5 (:senha)");
+  $stmt->bindParam(':email', $email);
+  $stmt->bindParam(':senha', $senha);
   $stmt->execute();
 
   // set the resulting array to associative
   $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-  foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
-    echo $v;
-  }
+  print_r(|$result)
 }
 } catch(PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
